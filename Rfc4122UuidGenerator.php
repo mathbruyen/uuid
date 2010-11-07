67a87490b86679d85b9a3d10111367d82fe83fe4
Mathieu Bruyen <code@mais-h.eu>
2010-11-07 18:51:24 +0100

    Add a basic class for generating RFC4122 UUIDs
    This class only defines and adds the tag for identifying RFC4122
    generators. The actual implementation is up to subclasses.

---
+++ Rfc4122UuidGenerator.php
@@ -0,0 +1,108 @@
+<?php
+
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+/**
+ * Definition of the base class for generating RFC4222 UUIDs
+ *
+ * PHP version 5
+ *
+ * Copyright (c) 2010, Mathieu Bruyen <code@mais-h.eu>.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in
+ *     the documentation and/or other materials provided with the
+ *     distribution.
+ *
+ *   * Neither the name of Mathieu Bruyen nor the names of his
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @category  Structures
+ * @package   UUID
+ * @author    Mathieu Bruyen <code@mais-h.eu>
+ * @copyright 2010 Mathieu Bruyen <code@mais-h.eu>
+ * @license   http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @version   SVN: $Id$
+ * @link      http://www.mais-h.eu/doc/index.php/UUID_php_package
+ * @since     File available since Release 1.0
+ */
+
+// Load interface
+require_once realpath(dirname(__FILE__)) . '/BaseUuidGenerator.php';
+
+// Load generator capacities
+require_once realpath(dirname(__FILE__)) . '/GeneratorCapacities.php';
+
+/**
+ * The basic class for RFC4122 UUID generators
+ * 
+ * It simply defines and adds the tag for RFC4122 UUIDs and actual UUID generation
+ * is up to subclasses.
+ * 
+ * @category  Structures
+ * @package   UUID
+ * @author    Mathieu Bruyen <code@mais-h.eu>
+ * @copyright 2010 Mathieu Bruyen <code@mais-h.eu>
+ * @license   http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @version   Release: @package_version@
+ * @link      http://www.mais-h.eu/doc/index.php/UUID_php_package
+ * @since     Interface available since Release 1.0
+ */
+abstract class UUID_Rfc4122UuidGenerator extends UUID_BaseUuidGenerator
+{
+    
+    /**
+     * The tag used for RFC4122 UUIDs
+     * 
+     * @var string
+     */
+    const TAG_RFC4122_UUID = 'Rfc4122';
+    
+    /**
+     * Constructor
+     *
+     * Calls the parent constructor and add the RFC4122 UUID tag.
+     * 
+     * @return void
+     *
+     * @access public
+     * @since Method available since Release 1.0
+     */
+    public function __construct()
+    {
+        parent::__construct();
+        $this->addTag(self::TAG_RFC4122_UUID);
+    }
+}
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * c-hanging-comment-ender-p: nil
+ * End:
+ */
+
+?>
