db78be0f4ec2fae77ac0ffa78c65d8144e8e3523
Mathieu Bruyen <code@mais-h.eu>
2010-11-07 15:33:33 +0100

    Add small script to search for TODOs, run tests and phpcs

---
+++ qualityinsurance.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Number of TODOs in the code
+echo "-----------------------------------------------------------------"
+totalTodoNumber=0
+totalFiles=0
+for f in $(ls *.php test/*.php)
+do
+    todoNumber=$(cat $f | grep TODO | wc -l)
+    if [ $todoNumber -gt 0 ]
+    then
+        echo "$f has $todoNumber TODO(s)"
+        totalTodoNumber=$(($todoNumber + $totalTodoNumber))
+        totalFiles=$(($totalFiles+1))
+    fi
+done
+echo
+echo "Total: $totalTodoNumber TODO(s) in $totalFiles file(s)"
+echo "-----------------------------------------------------------------"
+echo
+
+# PhpUnit with coverage
+phpunit --coverage-html ./report test/
+
+# CodeSniffer
+phpcs --standard=PEAR *.php test/*.php
